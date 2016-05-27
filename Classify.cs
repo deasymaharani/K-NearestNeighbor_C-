997e8b10b3f219869c087de89266bf720bc924c7
deasymaharani <deasylver@gmail.com>
2016-05-27 21:16:12 +1000

    knn

---
+++ Classify.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KNN
+{
+    public class Classify
+    {
+        List<double>[] dataTrain = new List<double>[4000];
+        List<double>[] dataTest = new List<double>[2000];
+        int k, dimension, sizeTrain, sizeTest;
+        public double akurasi;
+
+        List<KeyValuePair<double, int>>[] neighbors = new List<KeyValuePair<double, int>>[4000];
+        
+        public Classify()
+        { }
+        
+        public Classify(List<double>[] dataTrain, List<double>[] dataTest, int k, int dimension, int sizeTrain, int sizeTest)
+        {
+            this.dataTrain = dataTrain;
+            this.dataTest = dataTest;
+            this.k = k;
+            this.dimension = dimension;
+            this.sizeTrain = sizeTrain;
+            this.sizeTest = sizeTest;
+        }
+
+        static int Compare1(KeyValuePair<double, int> a, KeyValuePair<double, int> b)
+        {
+            return a.Key.CompareTo(b.Key);
+        }
+
+        static int Compare2(KeyValuePair<double, int> a, KeyValuePair<double, int> b)
+        {
+            return a.Value.CompareTo(b.Value);
+        }
+
+        public double EuclideanDistance(int a, int b, int dimension)
+        {
+            double diff, sumSquare = 0, sumSquareRoot;
+            double[] diffs = new double[10000];
+
+            for (int j = 0; j < dimension; j++)
+            {
+                diff = dataTest[a][j] - dataTrain[b][j];
+                diffs[j] = diff;
+            }
+
+            for (int i = 0; i < dimension; i++)
+            {
+                double square = Math.Pow(diffs[i], 2);
+                sumSquare += square;
+            }
+
+            sumSquareRoot = Math.Sqrt(sumSquare);
+
+            return sumSquareRoot;
+        }
+
+        public void NearestNeighbor()
+        {
+            int benar=0;
+            double distance;
+            for (int i = 0; i < sizeTest; i++)
+            {
+                neighbors[i] = new List<KeyValuePair<double, int>>();
+
+                for (int j = 0; j < sizeTrain; j++)
+                {
+                    if (i != j)
+                    {
+                        distance = EuclideanDistance(i,j,dimension);
+                        neighbors[i].Add(new KeyValuePair<double, int>(distance, j));
+                    }
+                }
+                neighbors[i].Sort(Compare1);
+            }
+
+            Console.WriteLine("in");
+            
+            List<int>[] nearestNeighbor = new List<int>[2000];
+
+            int[] count = new int[sizeTrain]; 
+            List<KeyValuePair<double, int>>[] detClass = new List<KeyValuePair<double, int>>[sizeTest];
+            for (int i = 0; i < sizeTest; i++)
+            {
+                for (int m = 0; m < sizeTrain; m++)
+                {
+                    count[m] = 0;
+                }
+                
+                nearestNeighbor[i] = new List<int>();
+                detClass[i] = new List<KeyValuePair<double, int>>();
+                for (int j = 0; j < k; j++)
+                {
+                    nearestNeighbor[i].Add(neighbors[i][j].Value);
+                    double temp = dataTrain[nearestNeighbor[i][j]][dimension];
+                    int temps = (int)temp;
+                    double counter = count[(int)temp]++;
+                    detClass[i].Add(new KeyValuePair<double, int>(counter, temps));
+                }
+
+                detClass[i].Sort(Compare1);
+
+                if (dataTest[i][dimension] == detClass[i][k - 1].Value)
+                { benar++; } 
+
+                dataTest[i][dimension] = detClass[i][k-1].Value;
+            }
+
+            double b = Convert.ToDouble(benar);
+            double st = Convert.ToDouble(sizeTest);
+            akurasi = (b / st)*100;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Timers;
+
+namespace KNN
+{
+    class Program
+    {
+        public static List<double>[] shuffleArray(List<double>[] dataInput, int sizeInput)
+        {
+            List<double>[] newDataInput = new List<double>[4000];
+
+            int angka = 0;
+            int i = 0; int[] flag = new int[4000]; int masuk=0;
+            while (masuk < sizeInput)
+            {
+                angka = angka + 2;
+                if (angka >= sizeInput )
+                {
+                    angka = 1;
+                }
+                if (masuk == sizeInput-1)
+                {
+                    angka = 0;
+                }
+                if (flag[angka] != 1)
+                {
+                    masuk++;
+                    flag[angka] = 1;
+                    newDataInput[i] = dataInput[angka];
+                    Console.WriteLine(i +" "+ angka);
+                    i++;
+                }
+            }
+
+            return newDataInput;
+        }
+
+        static void Main(string[] args)
+        {
+            List<double>[] dataInput = new List<double>[4000];
+            List<double>[] shuffleInput = new List<double>[4000];
+            List<double>[] dataTrain = new List<double>[4000];
+            List<double>[] dataTest = new List<double>[2000];
+            double inp = 0.0;
+
+            int sizeTrain = 0, sizeTest = 0, k, sizeInput = 0;
+
+            Console.WriteLine("Enter k : ");
+            k = int.Parse(Console.ReadLine());
+
+            //double x = 8.5;
+            //Console.WriteLine(x);
+
+            string[] linesTrain = System.IO.File.ReadAllLines(@"D:\DataAll.csv");
+
+            foreach (string line in linesTrain)
+            {
+                string[] aftersplit = line.Split(',');
+
+                dataInput[sizeInput] = new List<double>();
+                foreach (string num in aftersplit)
+                {
+
+                   inp = Convert.ToDouble(num.Replace('.', ','));
+                   dataInput[sizeInput].Add(inp);
+                }
+                
+                sizeInput++;
+            }
+
+            //string[] linesTest = System.IO.File.ReadAllLines(@"D:\dataTestSepeda.txt");
+
+            //foreach (string line in linesTest)
+            //{
+            //    string[] aftersplit = line.Split(',');
+
+            //    dataTest[sizeTest] = new List<double>();
+            //    foreach (string num in aftersplit)
+            //    {
+
+            //        inp = Convert.ToDouble(num.Replace('.', ','));
+            //        dataTest[sizeTest].Add(inp);
+            //    }
+
+            //    sizeTest++;
+            //}
+
+            //shuffleInput = shuffleArray(dataInput, sizeInput);
+
+            //Console.ReadKey();
+            int dimension = dataInput[0].Count - 1;
+
+
+            double sizeTrainD = Math.Ceiling(0.73 * sizeInput);
+            sizeTrain = Convert.ToInt32(sizeTrainD);
+            sizeTest = sizeInput - sizeTrain;
+
+            for (int i = 0; i < sizeTrain; i++)
+            {
+                dataTrain[i] = dataInput[i];
+
+            }
+            int idxTest = sizeTrain;
+            for (int i = 0; i < sizeTest; i++)
+            {
+                dataTest[i] = dataInput[idxTest];
+                idxTest++;
+            }
+            idxTest--;
+
+
+             //using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"D:\cekAll.txt"))
+             //   {
+
+             //       for (int i = 0; i < sizeInput; i++)
+             //       {
+             //           for (int j = 0; j < shuffleInput[i].Count; j++)
+             //           {
+             //               file.Write(shuffleInput[i][j] + " ");
+             //           }
+             //           file.WriteLine();
+             //       }
+             //   }
+
+            //using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"D:\cekTrainTest.txt"))
+            //{
+            //    file.WriteLine("Data Train");
+            //    for (int i = 0; i < sizeTrain; i++)
+            //    {
+            //        for (int j = 0; j < dataTrain[i].Count; j++)
+            //        {
+            //            file.Write(dataTrain[i][j] + " ");
+            //        }
+            //        file.WriteLine();
+            //    }
+
+            //    file.WriteLine("Data Test");
+            //    for (int i = 0; i < sizeTest; i++)
+            //    {
+            //        for (int j = 0; j < dataTest[i].Count; j++)
+            //        {
+            //            file.Write(dataTest[i][j] + " ");
+            //        }
+            //        file.WriteLine();
+            //    }
+            //}
+
+            Classify knn = new Classify(dataTrain, dataTest, k, dimension, sizeTrain, sizeTest);
+
+            knn.NearestNeighbor();
+
+
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"D:\outputSensor2.txt"))
+            {
+
+                for (int i = 0; i < sizeTest; i++)
+                {
+                    file.Write(dataTest[i][dimension] + " ");
+                    file.WriteLine();
+                }
+                file.WriteLine(knn.akurasi + "%");
+            }
+        }
+    }
+}
